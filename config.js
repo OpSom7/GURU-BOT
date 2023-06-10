7a28d904fd6c9835f313e8da54d6d8af5aff4f66
OpSom7 <118483612+OpSom7@users.noreply.github.com>
2023-06-10 19:20:59 +0530

    config.js

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = 'H 𝙸 𝙽 𝙰 𝚃 𝙰   ꢺ ᴀ ϻ ᴀ ﹤ᴣ'
 global.premium = 'true'
-global.packname = '   ㅤㅤ   ㅤ  ㅤ   👒ʜᴀɴᴅᴄʀᴀғᴛᴇᴅ ʙʏ :                                              ㅤㅤ                                                              ㅤㅤ                𓄂⍣⃝🇳ᴇꪎꪎᴇʀ  ꢺᴀϻᴀ妥' 
+global.packname = '   ㅤㅤ   ㅤ  ㅤ   👒ʜᴀɴᴅᴄʀᴀғᴛᴇᴅ ʙʏ :                                              ㅤㅤ                𓄂⍣⃝🇳ᴇꪎꪎᴇʀ  ꢺᴀϻᴀ妥' 
 global.author = 'Ɲᴇꪎꪎᴇʀ  ꢺᴀϻᴀ 𓆩×͜×𓆪ꪾ' 
 global.igfg = '▢ Follow on Instagram\nhttps://instagram.com/_just_an_weeb_?igshid=ZGUzMzM3NWJiOQ==\n' 
 global.dygp = 'https://chat.whatsapp.com/DF3fnIHbFxWEY3bqUAf7Is'
